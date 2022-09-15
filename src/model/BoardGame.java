95facfe658d897adbc4eb9ee6f767238aca58540
AlejandroOsejo <alejandrosejo@outlook.com>
2022-09-15 14:49:49 -0500

    BoardGame V1

--- src/model/BoardGame.java
+++ src/model/BoardGame.java
@@ -1,13 +1,39 @@
 package model;
 
 public class BoardGame {
-    private Casilla fuente;
-    private Casilla drenaje;
+    private Casilla head;
+    private Casilla tail;
+    private String[][] board; //tablero con matriz o con lista multiple enlazada
 
-    public void putPipe(){
+    public void generateBoard(){ //generar el board --> lista enlazada multiple
+        for (int i=0; i<8; i++){
+
+
+            for (int u=8; i>0; i--){
+
+            }
+        }
+
 
     }
-    public void simulate(){
+
+
+    public void showBoaard(){ // mostrar board --> 2 ciclos for
+
+    }
+
+
+    public void putPipe(){ // poner tubo
+
+    }
+
+
+    public void simulate(){ //Simular juego --> recursividad
+
+    }
+
+
+    public void calculateScore(){
 
     }
 
--- src/model/Casilla.java
+++ src/model/Casilla.java
@@ -2,7 +2,12 @@ package model;
 
 public class Casilla {
     private Casilla next;
-    private TipoTuberia tipo;
+    private Casilla prev;
+    private Casilla up;
+    private Casilla down;
+    private TipoTuberia tipo; // ||, =, o, source, drain
+    private int row;
+    private int col;
 
     public Casilla getNext() {return next;}
     public void setNext(Casilla next) {this.next = next;}

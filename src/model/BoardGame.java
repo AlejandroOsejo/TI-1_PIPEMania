5caec0f59a7620ef428d92b45184d0d930c41244
Vane155 <gloriavavi@gmail.com>
2022-09-16 19:47:56 -0500

    commit glori1

--- src/model/BoardGame.java
+++ src/model/BoardGame.java
@@ -1,12 +1,27 @@
 package model;
+import java.util.Random;
 
 public class BoardGame {
     private Casilla head;
     private Casilla tail;
     private String[][] board; //tablero con matriz
+    private Random random;
+
+
+    public BoardGame(){
+        board = new String[8][8];
+    }
 
     public void generateBoard(){ //generar el board
-        board = new String[8][8];
+
+        for (int x = 0; x<board.length;x++){
+            for(int y = 0; y<board[x].length;y++){
+                board[x][y]="x";
+            }
+        }
+        board[random.nextInt(8)][random.nextInt(8)]="F";
+        board[random.nextInt(8)][random.nextInt(8)]="D";
+
 
         
 
